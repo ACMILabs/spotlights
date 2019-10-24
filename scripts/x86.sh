1ca1103f1b6c57332cc46486129be66534855573
Harry <harry@boojum.com.au>
2019-10-24 15:32:30 +1100

    forgot to put no-sandbox back

--- scripts/x86.sh
+++ scripts/x86.sh
@@ -13,7 +13,7 @@ python -u app/main.py &
 
 sleep 5
 
-chromium --app=http://localhost:8080 --enable-accelerated-video --enable-accelerated-mjpeg-decode --ignore-gpu-blacklist --enable-gpu-rasterization --enable-zero-copy --enable-native-gpu-memory-buffers --test-type --start-fullscreen --user-data-dir --kiosk --disable-application-cache --incognito
+chromium --app=http://localhost:8080 --enable-accelerated-video --enable-accelerated-mjpeg-decode --ignore-gpu-blacklist --enable-gpu-rasterization --enable-zero-copy --enable-native-gpu-memory-buffers --test-type --start-fullscreen --user-data-dir --kiosk --disable-application-cache --incognito --no-sandbox
 
 # Use this for remote debug
 #chromium --no-sandbox --disable-gpu --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --headless http://localhost:8080

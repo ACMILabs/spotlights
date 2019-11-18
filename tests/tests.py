c48c89b88ca699dbbd951af570414eb73d9ca24d
Harry <harry@boojum.com.au>
2019-11-18 16:37:56 +1100

    test taps and xos playlist parsing

--- tests/tests.py
+++ tests/tests.py
@@ -10,6 +10,8 @@ from app.main import Label
 
 from app.cache import create_cache
 
+XOS_PLAYLIST_ID = os.environ['XOS_PLAYLIST_ID']
+
 @pytest.fixture
 def database():
     """
@@ -44,9 +46,9 @@ def test_label(database):
 
 
 class MockJsonResponse:
-    def __init__(self, data):
+    def __init__(self, data, status_code):
         self.content = json.loads(data)
-        self.status_code = 200
+        self.status_code = status_code
     def json(self):
         return self.content
 
@@ -65,7 +67,7 @@ class MockTextResponse:
 def mocked_requests_get(*args, **kwargs):
     if '/api/playlists/' in args[0]:
         with open('tests/data/playlist.json', 'r') as file_obj:
-            return MockJsonResponse(file_obj.read())
+            return MockJsonResponse(file_obj.read(), 200)
 
     if '.mp4' in args[0]:
         with open('tests/data/sample.mp4', 'rb') as file_obj:
@@ -82,6 +84,29 @@ def mocked_requests_get(*args, **kwargs):
     raise Exception("No mocked sample data for request: "+args[0])
 
 
+def mocked_requests_post(*args, **kwargs):
+    if 'https://' in args[0] and '/api/taps/' in args[0]:
+        with open('tests/data/xos_tap.json', 'r') as f:
+            return MockJsonResponse(f.read(), 201)
+
+    raise Exception("No mocked sample data for request: "+args[0])
+
+
+
+
+@patch('requests.post', side_effect=mocked_requests_post)
+def test_route_collect_item(mocked_requests_post, client):
+    """
+    Test that the collect a tap route forwards the expected data to XOS.
+    """
+
+    with open('tests/data/lens_tap.json', 'r') as f:
+        lens_tap_data = f.read()
+
+    response = client.post('/api/taps/', data=lens_tap_data, headers={'Content-Type': 'application/json'})
+    assert response.json["nfc_tag"]["short_code"] == "nbadbb"
+    assert response.status_code == 201
+
 
 @patch('requests.get', side_effect=mocked_requests_get)
 def test_cache(capsys):
@@ -91,6 +116,10 @@ def test_cache(capsys):
     # capsys.disabled forwards stdout and stderr
     with capsys.disabled():
         create_cache()
+        with open('playlist_'+XOS_PLAYLIST_ID+'.json', 'r') as f:
+            playlist = json.loads(f.read())['playlist_labels']
+        assert len(playlist) == 2
+        assert playlist[0]['label']['title'] == 'Placeholder video 1'
 
 
 def test_index_renders(client):

d60afaf3eaa550b8d564895c4e424b61dd1776de
Harry <harry@boojum.com.au>
2020-01-24 15:53:09 +1100

    1481 create hastapped table in test

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,9 +18,9 @@ def database():
     Setup the test database.
     """
     test_db = SqliteDatabase(':memory:')
-    test_db.bind([Label], bind_refs=False, bind_backrefs=False)
+    test_db.bind([Label, HasTapped], bind_refs=False, bind_backrefs=False)
     test_db.connect()
-    test_db.create_tables([Label])
+    test_db.create_tables([Label, HasTapped])
 
     Label.create(
         datetime=datetime.datetime.now().timestamp(),

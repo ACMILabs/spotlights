956a2cf326b5bfe0b0719609c0c4aca9da72879d
Harry <harry@boojum.com.au>
2020-01-29 10:33:08 +1100

    lint js tests

--- tests/js/__tests__/spotlights.test.js
+++ tests/js/__tests__/spotlights.test.js
@@ -1,52 +1,45 @@
-global.EventSource = function () {
-  return
-}
+global.EventSource = function dummyEventSource() {};
+global.playlist_labels = require("../../data/playlist.json").playlist_labels;
+
+document.body.innerHTML = "<div id='root'/>";
+require("../../../app/static/index.js");
 
 describe("App", () => {
-  beforeAll(() => {
-    global.playlist_labels = (require('../../data/playlist.json')).playlist_labels
-    document.body.innerHTML = "<div id='root'/>"
-
-    require("../../../app/static/index.js")
-  });
-
+  beforeAll(() => {});
 
   it("doesn't crash from clicking any divs", () => {
-    const elements = document.getElementsByTagName('div')
-    for (let i=0; i<elements.length; i++) {
-      elements[i].dispatchEvent(new MouseEvent('click'))
+    const elements = document.getElementsByTagName("div");
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].dispatchEvent(new MouseEvent("click"));
     }
   });
-
 
   it("doesn't crash from dragging any divs", () => {
-    const elements = document.getElementsByTagName('div')
-    for (let i=0; i<elements.length; i++) {
-      elements[i].dispatchEvent(new MouseEvent('mousedown', {clientX: 0}))
-      elements[i].dispatchEvent(new MouseEvent('mousemove', {clientX: -100}))
-      elements[i].dispatchEvent(new MouseEvent('mouseup'))
-      elements[i].dispatchEvent(new MouseEvent('click'))
+    const elements = document.getElementsByTagName("div");
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].dispatchEvent(new MouseEvent("mousedown", { clientX: 0 }));
+      elements[i].dispatchEvent(new MouseEvent("mousemove", { clientX: -100 }));
+      elements[i].dispatchEvent(new MouseEvent("mouseup"));
+      elements[i].dispatchEvent(new MouseEvent("click"));
     }
   });
 
-
   it("doesn't crash from dragging the video list", () => {
-    const list_cont = document.getElementsByClassName('list_cont')[0]
-    list_cont.dispatchEvent(new MouseEvent('mousedown', {clientX: 0}))
-    list_cont.dispatchEvent(new MouseEvent('mousemove', {clientX: 0}))
-    list_cont.dispatchEvent(new MouseEvent('mousemove', {clientX: -100}))
-    list_cont.dispatchEvent(new MouseEvent('mouseup'))
+    const list_cont = document.getElementsByClassName("list_cont")[0];
+    list_cont.dispatchEvent(new MouseEvent("mousedown", { clientX: 0 }));
+    list_cont.dispatchEvent(new MouseEvent("mousemove", { clientX: 0 }));
+    list_cont.dispatchEvent(new MouseEvent("mousemove", { clientX: -100 }));
+    list_cont.dispatchEvent(new MouseEvent("mouseup"));
   });
 
-
   it("doesn't crash on resize", () => {
-    window.dispatchEvent(new Event('resize'))
+    window.dispatchEvent(new Event("resize"));
   });
 
   it("automatically plays next video", () => {
-    const video = document.getElementsByTagName('video')[0]
-    const initial_video_src = video.src
-    video.dispatchEvent(new Event('ended'))
-    expect(video.src).not.toBe(initial_video_src)
-  })
-})
+    const video = document.getElementsByTagName("video")[0];
+    const initial_video_src = video.src;
+    video.dispatchEvent(new Event("ended"));
+    expect(video.src).not.toBe(initial_video_src);
+  });
+});

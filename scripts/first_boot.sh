bf7043192f3cab621a77146a4c0c2e072ccb6372
Simon Loffler <simon.loffler@acmi.net.au>
2021-01-28 14:57:49 +1030

    #58 WIP curl first boot container for boot status

--- scripts/first_boot.sh
+++ scripts/first_boot.sh
@@ -2,8 +2,10 @@
 
 echo "________Start of first_boot.sh________"
 
-# TODO: If boot-count == 1, restart the container
-if [[  ]]; then
+BOOT_COUNT_RESPONSE=$(curl -sk http://bootcount:8082)
+BOOT_COUNT=$(echo $BOOT_COUNT_RESPONSE | jq --raw-output '.count')
+
+if [[ $BOOT_COUND == "1" ]]; then
   # Restart the container
     echo "This is the first boot, so restarting the spotlights container to resolve the lack of touch input..."
     curl -H "Content-Type: application/json" -d "{\"serviceName\": \"$BALENA_SERVICE_NAME\"}" "$BALENA_SUPERVISOR_ADDRESS/v2/applications/$BALENA_APP_ID/restart-service?apikey=$BALENA_SUPERVISOR_API_KEY"

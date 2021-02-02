362ae7596a76885b1ab1f444adf53ea49709c4e9
Simon Loffler <simon.loffler@acmi.net.au>
2021-02-02 13:11:26 +1100

    #58 Fix typo

--- scripts/first_boot.sh
+++ scripts/first_boot.sh
@@ -5,10 +5,12 @@ echo "________Start of first_boot.sh________"
 BOOT_COUNT_RESPONSE=$(curl -sk http://bootcount:8082/api/count/)
 BOOT_COUNT=$(echo $BOOT_COUNT_RESPONSE | jq --raw-output '.count')
 
-if [[ $BOOT_COUND == "1" ]]; then
+if [[ $BOOT_COUNT == "1" ]]; then
   # Restart the container
-    echo "This is the first boot, so restarting the spotlights container to resolve the lack of touch input..."
-    curl -H "Content-Type: application/json" -d "{\"serviceName\": \"$BALENA_SERVICE_NAME\"}" "$BALENA_SUPERVISOR_ADDRESS/v2/applications/$BALENA_APP_ID/restart-service?apikey=$BALENA_SUPERVISOR_API_KEY"
+  echo "This is the first boot, so restarting the spotlights container to resolve the lack of touch input..."
+  curl -H "Content-Type: application/json" -d "{\"serviceName\": \"$BALENA_SERVICE_NAME\"}" "$BALENA_SUPERVISOR_ADDRESS/v2/applications/$BALENA_APP_ID/restart-service?apikey=$BALENA_SUPERVISOR_API_KEY"
+else
+  echo "Boot count is $BOOT_COUNT so not restarting..."
 fi
 
 echo "________End of first_boot.sh________"
